4e476251880b369ae7464b61b53b012ebf885931
VP <VP>
2021-09-03 19:11:59 +0300

    commit #1

--- SwiftUIScrollViewReader/ContentView.swift
+++ SwiftUIScrollViewReader/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollViewReader { scroolView in
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Button(action: {
+                            withAnimation {
+                                scroolView.scrollTo(100,
+                                                    anchor: .center)
+                            }
+                        }, label: {
+                            Text("Scroll to Bottom")
+                        })
+                        ForEach(0...100, id: \.self) { num in
+                            HStack {
+                                Label(
+                                    title: { Text("Position: \(num)") },
+                                    icon: { Image(systemName: "house") }
+                                )
+                                Spacer()
+                            }
+                            .id(num)
+                            .padding()
+                            }
+                        }
+                    }
+                    .navigationTitle("Scroll View")
+            }
+        }
     }
 }
 
