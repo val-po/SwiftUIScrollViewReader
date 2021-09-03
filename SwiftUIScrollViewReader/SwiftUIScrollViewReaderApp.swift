cdcd5bce3ca2e2e24edb32682d460a926b389202
VP <VP>
2021-09-03 18:29:26 +0300

    Initial Commit

---
+++ SwiftUIScrollViewReader/SwiftUIScrollViewReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewReaderApp.swift
+//  SwiftUIScrollViewReader
+//
+//  Created by valentine on 03.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
